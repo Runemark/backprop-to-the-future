b773108db6c0ebf0588dd4a9993926f7b732c98e
Martin Mumford <Martin@Martins-MBP.home>
2015-02-21 07:33:41 -0700

    Project Initialization

---
+++ recurrent/recurrent/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  recurrent
+//
+//  Created by Martin Mumford on 2/21/15.
+//  Copyright (c) 2015 Runemark Studios. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
